9b20b70d930b901067069bd4bc79ea8c47c30a60
Riley Durbin <radurbin@crimson.ua.edu>
2023-09-30 22:51:13 -0500

    + Finished required elements and functions

--- Fall2023-Assignment2-radurbin/wwwroot/script.js
+++ Fall2023-Assignment2-radurbin/wwwroot/script.js
@@ -1,16 +1,16 @@
 /*
 JS:
-    - Replace "my-api-url" under the ajax call with the url from your search API.
-    - Replace "my-api-key" next to "Ocp-Apim-Subscription-Key" with the api key from your search API.
-    - Write a function that calls the "apiSearch" function on a click of your search button.
-    - Write a function that changes the background image of your site on a click of your search engine name.
-    - Write a function that gets the current time (formatted HH:MM), loads the result into your 'time' div, and displays the div as a jQueryUI dialog window on a click of your time button.
+    ✔ Replace "my-api-url" under the ajax call with the url from your search API.
+    ✔ Replace "my-api-key" next to "Ocp-Apim-Subscription-Key" with the api key from your search API.
+    ✔ Write a function that calls the "apiSearch" function on a click of your search button.
+    ✔ Write a function that changes the background image of your site on a click of your search engine name.
+    ✔ Write a function that gets the current time (formatted HH:MM), loads the result into your 'time' div, and displays the div as a jQueryUI dialog window on a click of your time button.
 BONUS:
-    - Implement a "I'm feeling lucky" button. This button should take the user to the first web page returned from your search API.
-    - Modify the function that changes your background image so that each click cycles through two different images without refreshing the page.
+    ✔ Implement a "I'm feeling lucky" button. This button should take the user to the first web page returned from your search API.
+    ✔ Modify the function that changes your background image so that each click cycles through two different images without refreshing the page.
 */
 
-function apiSearch() {
+function apiSearch(lucky) {
     var params = {
         'q': $('#query').val(),
         'count': 50,
@@ -26,19 +26,53 @@ function apiSearch() {
         }
     })
         .done(function (data) {
-            var len = data.webPages.value.length;
-            var results = '';
-            for (i = 0; i < len; i++) {
-                results += `<p><a href="${data.webPages.value[i].url}">${data.webPages.value[i].name}</a>: ${data.webPages.value[i].snippet}</p>`;
-            }
 
-            $('#searchResults').html(results);
-            $('#searchResults').dialog();
+            if (!lucky) {
+                var len = data.webPages.value.length;
+                var results = '';
+                for (i = 0; i < len; i++) {
+                    results += `<p><a href="${data.webPages.value[i].url}">${data.webPages.value[i].name}</a>: ${data.webPages.value[i].snippet}</p>`;
+                }
+
+                $('#searchResults').html(results);
+                //$('#searchResults').dialog();
+            }
+            else {
+                window.location.href = data.webPages.value[0].url;
+            }
         })
         .fail(function () {
             alert('error');
         });
 }
 
-console.log("test");
-console.log(document.body.style.backgroundImage);
+$("#searchButton").click(function () {
+    document.getElementById("searchResults").style.visibility = "visible";
+    apiSearch(false);
+});
+
+$('#feelingLucky').click(function () {
+    apiSearch(true);
+});
+
+var toggle = true;
+
+$('#title').click(function () {
+    if (toggle) {
+        $("body").css("background-image", "url('./css/images/fillerbg.jpeg')");
+    }
+    else {
+        $("body").css("background-image", "url('./css/images/background.webp')");
+    }
+    toggle = !toggle;
+});
+
+$('#timeButton').click(function () {
+    document.getElementById("time").style.visibility = "visible";
+    var curTime = new Date();
+    var hours = curTime.getHours().toString().padStart(2, "0");
+    var minutes = curTime.getMinutes().toString().padStart(2, "0");
+    var formattedTime = '' + hours + ':' + minutes;
+    $('#time').html('Current Time: ' + formattedTime);
+    $('#time').dialog();
+});
